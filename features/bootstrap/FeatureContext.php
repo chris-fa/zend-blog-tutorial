2c42551b1bc710a80767b9e4881569eb58b0e5bd
= <=>
2016-09-26 17:00:26 -0500

    Added boostrap file

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -3,14 +3,32 @@
 use Behat\Behat\Tester\Exception\PendingException;
 use Behat\MinkExtension\Context\MinkContext;
 
+/* define('APPLICATION_ENV', 'testing'); */
+/* define('APPLICATION_PATH', dirname(__FILE__) . '/../..'); */
+
+/* set_include_path('.' . PATH_SEPARATOR . APPLICATION_PATH . '/../library' */
+/*     . PATH_SEPARATOR . get_include_path()); */
+
+require_once dirname(__FILE__) . '/../application/ControllerTestCase.php';
+
+/* use Blog\AcceptanceTests\ControllerTestCase; */
+
 class FeatureContext extends MinkContext
 {
+    protected $app;
+
+    public function __construct()
+    {
+        $this->app = new ControllerTestCase();
+        $this->app->setUp();
+    }
 
     /**
      * @Given There are :arg1 blog entries
      */
     public function thereAreBlogEntries($arg1)
     {
-        throw new PendingException();
+        $a = $this->app->db();
+        $var_dump($a);
     }
 }
