4a8dd4e6e80eb7131bcc7cfc10db393fe37a2b80
= <=>
2016-09-28 10:53:02 -0500

    Add missing doctrine configuration

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,5 +12,22 @@
  */
 
 return array(
-    // ...
+    'doctrine' =>  [
+        'connection' => [
+            'orm_default' => [
+                'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
+                'params' => [
+                    'host' => 'localhost',
+                    'port' => 3306,
+                    'user' => 'root',
+                    'password' => '',
+                    'dbname' => 'blog_tutorial',
+                    'encoding' => 'utf8',
+                    'driverOptions' => [
+                        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+                    ],
+                ],
+            ],
+        ],
+    ],
 );
